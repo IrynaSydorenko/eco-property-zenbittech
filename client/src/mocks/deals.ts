b5213127773478603737abaa83d7655737e45853
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-10-21 12:55:38 +0200

    test: deals mock data

---
+++ client/src/mocks/deals.ts
@@ -0,0 +1,44 @@
+import { Deal } from '../types';
+
+export const deals: Deal[] = [
+  {
+    id: 1,
+    title: 'The Marina Torch',
+    totalAmount: 6500000,
+    pricePerSqM: 60000,
+    amountSold: 75,
+    projectedYield: 9.25,
+    daysLeft: 150,
+    imageUrl: 'https://i.imgur.com/NFKN5Bt.png',
+  },
+  {
+    id: 2,
+    title: 'HHHR Tower',
+    totalAmount: 6500000,
+    pricePerSqM: 60000,
+    amountSold: 75,
+    projectedYield: 9.25,
+    daysLeft: 150,
+    imageUrl: 'https://i.imgur.com/ujBzam1.png',
+  },
+  {
+    id: 3,
+    title: 'Ocean Peaks',
+    totalAmount: 6500000,
+    pricePerSqM: 60000,
+    amountSold: 75,
+    projectedYield: 9.25,
+    daysLeft: 150,
+    imageUrl: 'https://i.imgur.com/Dgv49bz.png',
+  },
+  {
+    id: 4,
+    title: 'Al Yagoub Tower',
+    totalAmount: 6500000,
+    pricePerSqM: 60000,
+    amountSold: 75,
+    projectedYield: 9.25,
+    daysLeft: 150,
+    imageUrl: 'https://i.imgur.com/Y6DZQv0.png',
+  },
+];
