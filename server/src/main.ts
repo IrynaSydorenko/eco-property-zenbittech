7989fa221a3ad5bd3e3cd0dd49eaf5e21abf15d2
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-10-21 13:50:47 +0200

    feat: init structure of server nest

---
+++ server/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 5000);
+}
+bootstrap();
